8cd98a15ea95a08add669830a48139a4b3371745
ClaasBeger <claas_baller@yahoo.com>
2024-01-06 01:58:22 +0100

    Added encoding of surname and geography data

--- model.py
+++ model.py
@@ -108,10 +108,114 @@ train_df['CustomerId_zscore']=zscore_scaler.transform(train_df[['CustomerId']])
 
 train_df['CustomerId_zscore'].describe()
 
+train_df.drop('CustomerId', axis=1, inplace=True)
 
 #%%
 
 # One-Hot encoding of gender
 
-#train_df = pd.get_dummies(train_df, columns=['Gender'], drop_first=True)
+train_df = pd.get_dummies(train_df, columns=['Gender'], drop_first=True)
+
+#%%
+
+# Classify names by their origin using corresponding lookup table
+
+from names_dataset import NameDataset, NameWrapper
+
+nd = NameDataset()
+
+#%%
+
+# Add new column with Country of surname origin
+
+def lookUpOrigin(row):
+    entry = nd.search(row['Surname'])
+    if(entry['last_name'] == None):
+        return 'Unknown'
+    return next(iter(nd.search(row['Surname'])['last_name']['country'].keys()))
+
+train_df['Name_Origin'] = train_df.apply(lookUpOrigin, axis=1)
+
+# Next I would like to add a column that encodes the country origin information
+# through its location data 
+
+from geopy.geocoders import Nominatim
+from sklearn.manifold import MDS
+from sklearn.preprocessing import MinMaxScaler
+from geopy.distance import geodesic
+
+# List of countries for name origins which we want to retrieve coordinates for
+countries = train_df['Name_Origin'].unique()
+
+# Initialize geolocator
+geolocator = Nominatim(user_agent="country_locator")
+
+coordinates = {}
+
+# Retrieve coordinates for each country
+for country in countries:
+    location = geolocator.geocode(country)
+    if location:
+        coordinates[country] = (location.latitude, location.longitude)
+    else:
+        print(f"Coordinates not found for {country}")
+
+# Create DataFrame from coordinates
+#coordinate_df = pd.DataFrame(coordinates.values(), index=coordinates.keys(), columns=['Latitude', 'Longitude'])
+
+# Calculate distances between countries
+# =============================================================================
+# distances = {}
+# for country1, coord1 in coordinates.items():
+#     for country2, coord2 in coordinates.items():
+#         if country1 != country2 and (country2, country1) not in distances:
+#             dist = geodesic(coord1, coord2).kilometers
+#             distances[(country1, country2)] = dist
+# =============================================================================
+
+# Create a distance matrix
+# =============================================================================
+# distance_matrix = pd.DataFrame(0, index=coordinate_df.index, columns=coordinate_df.index)
+# for (country1, country2), dist in distances.items():
+#     distance_matrix.loc[country1, country2] = dist
+#     distance_matrix.loc[country2, country1] = dist
+# =============================================================================
+
+# Apply Multidimensional Scaling (MDS) for dimensionality reduction
+# =============================================================================
+# mds = MDS(n_components=1, dissimilarity='precomputed', random_state=42)
+# numeric_values = mds.fit_transform(distance_matrix)
+# =============================================================================
+
+# Scale the numeric values to a desired range
+# =============================================================================
+# scaler = MinMaxScaler()
+# coordinate_df['NumericCountry'] = scaler.fit_transform(numeric_values)
+# =============================================================================
+
+# Replace value for unknown by average
+
+tuples = [value for key, value in coordinates.items() if key != 'Unknown']
+
+# Calculate the averages for each component of the tuples
+avg_1 = sum(x[0] for x in tuples) / len(tuples)
+avg_2 = sum(x[1] for x in tuples) / len(tuples)
+
+# Update the 'Unknown' key with the averages
+coordinates['Unknown'] = (avg_1, avg_2)
+
+train_df['Surname_Origin_Latitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[0])
+train_df['Surname_Origin_Longitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[1])
+
+
+# NumericCountry does not reflect distance too well, instead I just insert Longitude and Latitute
+
+train_df['Geography_Latitude'] = train_df['Geography'].map(lambda x: coordinates[x][0])
+train_df['Geography_Longitude'] = train_df['Geography'].map(lambda x: coordinates[x][1])
+
+#%%
+
+# drop unneeded columns
+
+train_df.drop(labels=['Surname', 'Geography', 'Name_Origin'], axis=1, inplace=True)
 
