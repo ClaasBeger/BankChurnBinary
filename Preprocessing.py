f2dd8e34e425e2656e0e4f9e473be308015642ba
ClaasBeger <claas_baller@yahoo.com>
2024-01-11 03:37:05 +0100

    Finished first data preprocessing approach, completed first Naive_Bayes_Model

---
+++ Preprocessing.py
@@ -0,0 +1,275 @@
+import pandas as pd
+
+import torch
+
+import numpy as np
+
+train_df = pd.read_csv("train.csv")
+
+test_df = pd.read_csv("test.csv")
+
+#%%
+
+# Small data description (can be deleted later):
+    
+# =============================================================================
+# id - Unique ID. We can use this features as a count for other parameters.
+# 
+# CustomerID - Customer ID is not a unique ID, there are few customer IDs which have more than 80 repetitive occurrences.
+# 
+# Surname - This is the surname of the customer, there are too many repititions. Actually there are only around 2700 unique surnames.
+# 
+# Credit Score - Your creditworthiness is rated by a three-digit figure called a credit score. 300 to 850 is the range of FICO scores.
+# You have a better chance of getting approved for loans and better prices the higher your score. Now in our dataset there are range of values starting from 350 and going up to 850. Now this can be a very useful information while thinking about the churning.
+# 
+# Geography - There are 3 unique values - France, Spain, and Germany. One has to use Label Encoder or OneHotEncoder to encode these values.
+# 
+# Gender - There are only 2 unique values - Male and Female. Here a label binarizer is enough for the encoding purposes.
+# 
+# Age - Depicts the age of the customers. There are all possible values starting from 18 up to 92. 
+# There are 2 anomalies found in the age column - there 2 values in float - 32.44 and 36.44. It would be better if we can round those values to 32 and 36 respectively.
+# 
+# Tenure - It might show from how many years the customer has been related to the bank or may be vice versa. There are values ranging from 0 to 10.
+#  Most probably these values are in years.
+# 
+# Balance - This is the bank balance of the customer. There were many doubts in the discussion forum that the bank balance was 0. 
+# When I performed the analysis, I found that actually 89000+ people had 0 bank balance. While the maximum amount recorded was around 250,000.
+# 
+# Number of Products - Now this can be a very difficult question. While there are only 4 unique values possible - 1, 2, 3, and 4.
+#  This can be attributes to how many major/big products the customer owns. Or other explanation might be that how many products the customer has bought on loan.
+# 
+# Has Credit Card - Clear cut, whether the customer has a credit card or not. Same goes for the next column as well Is Active Member. 
+# 
+# Estimated Salary - What is the estimated salary of the individual. Now, this is a very important aspect of the real life scenario. Whenever you are given a credit from the bank,
+# they mostly ask for whether or not you are salaried. If you are estimated of getting a higher salary, easier for them to credit you a higher amount of loan.
+# =============================================================================
+
+#%%
+
+# Look for nan values and categorical variables
+
+train_df.info()
+
+# No null values
+
+print(train_df.applymap(np.isreal).all(0))
+
+# Identified Three Categorical Variables: Surname, Geography and Gender
+
+#%%
+
+train_df.head()
+
+#%%
+
+#Inspect different attributes
+
+train_df.id.describe()
+
+# ID is unique, seems to be relating to process (distinct applicant list also possible)
+
+#%%
+
+len(train_df.CustomerId.unique())
+
+# 23221 distinct values -> not unique (reuse possible, or multiple processes per Customer)
+
+# To investigate, we look at rows with same CustomerId
+
+train_df['CustomerId'].duplicated()
+
+#df = train_df[train_df.duplicated('CustomerId', keep=False)].sort_values('CustomerId')
+
+# Customer ID does not display any obvious pattern or relation to the respective process
+
+# I will replace the CustomerID with the respective count
+
+CustomerIdCounts = train_df['CustomerId'].value_counts()
+
+# Create a new column with counts based on CustomerId
+train_df['CustomerIdCounts'] = train_df['CustomerId'].map(CustomerIdCounts)
+
+# Drop the old 'CustomerId' column if needed
+train_df.drop(columns=['CustomerId'], inplace=True)
+
+# Rename the new column to 'CustomerId'
+train_df.rename(columns={'CustomerIdCounts': 'CustomerId'}, inplace=True)
+
+# Potentially use a zscaler to transform
+
+from sklearn import preprocessing
+
+# create a scaler using Z score normalization / Standardization on the Fare attribute
+# The Fare varies greatly, thus we want to normalize this attribute
+zscore_scaler = preprocessing.StandardScaler().fit(train_df[['CustomerId']])
+
+#Apply the Zscore scaler Fare attribute and assign to a new Zscore column
+train_df['CustomerId_zscore']=zscore_scaler.transform(train_df[['CustomerId']])
+
+train_df['CustomerId_zscore'].describe()
+
+train_df.drop('CustomerId', axis=1, inplace=True)
+
+#%%
+
+# One-Hot encoding of gender
+
+train_df = pd.get_dummies(train_df, columns=['Gender'], drop_first=True)
+
+#%%
+
+# Classify names by their origin using corresponding lookup table
+
+from names_dataset import NameDataset, NameWrapper
+
+nd = NameDataset()
+
+#%%
+
+# Add new column with Country of surname origin
+
+def lookUpOrigin(row):
+    entry = nd.search(row['Surname'])
+    if(entry['last_name'] == None):
+        return 'Unknown'
+    return next(iter(nd.search(row['Surname'])['last_name']['country'].keys()))
+
+train_df['Name_Origin'] = train_df.apply(lookUpOrigin, axis=1)
+
+# Next I would like to add a column that encodes the country origin information
+# through its location data 
+
+from geopy.geocoders import Nominatim
+from sklearn.manifold import MDS
+from sklearn.preprocessing import MinMaxScaler
+from geopy.distance import geodesic
+
+# List of countries for name origins which we want to retrieve coordinates for
+countries = train_df['Name_Origin'].unique()
+
+# Initialize geolocator
+geolocator = Nominatim(user_agent="country_locator")
+
+coordinates = {}
+
+# Retrieve coordinates for each country
+for country in countries:
+    location = geolocator.geocode(country)
+    if location:
+        coordinates[country] = (location.latitude, location.longitude)
+    else:
+        print(f"Coordinates not found for {country}")
+
+# Create DataFrame from coordinates
+#coordinate_df = pd.DataFrame(coordinates.values(), index=coordinates.keys(), columns=['Latitude', 'Longitude'])
+
+# Calculate distances between countries
+# =============================================================================
+# distances = {}
+# for country1, coord1 in coordinates.items():
+#     for country2, coord2 in coordinates.items():
+#         if country1 != country2 and (country2, country1) not in distances:
+#             dist = geodesic(coord1, coord2).kilometers
+#             distances[(country1, country2)] = dist
+# =============================================================================
+
+# Create a distance matrix
+# =============================================================================
+# distance_matrix = pd.DataFrame(0, index=coordinate_df.index, columns=coordinate_df.index)
+# for (country1, country2), dist in distances.items():
+#     distance_matrix.loc[country1, country2] = dist
+#     distance_matrix.loc[country2, country1] = dist
+# =============================================================================
+
+# Apply Multidimensional Scaling (MDS) for dimensionality reduction
+# =============================================================================
+# mds = MDS(n_components=1, dissimilarity='precomputed', random_state=42)
+# numeric_values = mds.fit_transform(distance_matrix)
+# =============================================================================
+
+# Scale the numeric values to a desired range
+# =============================================================================
+# scaler = MinMaxScaler()
+# coordinate_df['NumericCountry'] = scaler.fit_transform(numeric_values)
+# =============================================================================
+
+# Replace value for unknown by average
+
+tuples = [value for key, value in coordinates.items() if key != 'Unknown']
+
+# Calculate the averages for each component of the tuples
+avg_1 = sum(x[0] for x in tuples) / len(tuples)
+avg_2 = sum(x[1] for x in tuples) / len(tuples)
+
+# Update the 'Unknown' key with the averages
+coordinates['Unknown'] = (avg_1, avg_2)
+
+train_df['Surname_Origin_Latitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[0])
+train_df['Surname_Origin_Longitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[1])
+
+
+# NumericCountry does not reflect distance too well, instead I just insert Longitude and Latitute
+
+train_df['Geography_Latitude'] = train_df['Geography'].map(lambda x: coordinates[x][0])
+train_df['Geography_Longitude'] = train_df['Geography'].map(lambda x: coordinates[x][1])
+
+#%%
+
+# drop unneeded columns
+
+train_df.drop(labels=['Surname', 'Geography', 'Name_Origin'], axis=1, inplace=True)
+
+# Potentially investigate information loss through coordinate encoding
+
+#%%
+
+# Further normalization
+# Apply Zscore standardisation to CreditScore and add additional column with pre-defined credit range label
+
+def assignLabel(score):
+    if(score>799):
+        return 5
+    if(score>739):
+        return 4
+    if(score>669):
+        return 3
+    if(score>579):
+        return 2
+    return 1
+        
+
+train_df['CreditScore_Range'] = train_df["CreditScore"].map(lambda x: assignLabel(x))
+
+zscore_scaler = preprocessing.StandardScaler().fit(train_df[['CreditScore']])
+
+#Apply the Zscore scaler Fare attribute and assign to a new Zscore column
+train_df['CreditScore_zscore']=zscore_scaler.transform(train_df[['CreditScore']])
+
+train_df.drop('CreditScore', axis=1, inplace=True)
+
+#%%
+
+# Apply Zscore to Age
+zscore_scaler = preprocessing.StandardScaler().fit(train_df[['Age']])
+
+#Apply the Zscore scaler Fare attribute and assign to a new Zscore column
+train_df['Age_zscore']=zscore_scaler.transform(train_df[['Age']])
+
+train_df.drop('Age', axis=1, inplace=True)
+# Apply Zscore to Balance
+zscore_scaler = preprocessing.StandardScaler().fit(train_df[['Balance']])
+
+#Apply the Zscore scaler Fare attribute and assign to a new Zscore column
+train_df['Balance_zscore']=zscore_scaler.transform(train_df[['Balance']])
+
+train_df.drop('Balance', axis=1, inplace=True)
+# Apply Zscore to Estimated Salary
+zscore_scaler = preprocessing.StandardScaler().fit(train_df[['EstimatedSalary']])
+
+train_df['EstimatedSalary_zscore']=zscore_scaler.transform(train_df[['EstimatedSalary']])
+
+train_df.drop('EstimatedSalary', axis=1, inplace=True)
+
+#%%
+
+train_df.to_csv("test_cleaned.csv", index=False)
--- model.py
+++ model.py
@@ -1,221 +1,43 @@
 import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 
-import torch
+train_df = pd.read_csv("train_cleaned.csv")
 
-import numpy as np
+test_df = pd.read_csv("test_cleaned.csv")
 
-train_df = pd.read_csv("train.csv")
+y_train = train_df["Exited"]
+x_train = train_df.drop('Exited', axis=1)
 
-test_df = pd.read_csv("test.csv")
+x_test = test_df
 
-#%%
+# Initialize the Gaussian Naive Bayes classifier
+nb_classifier = GaussianNB()
 
-# Small data description (can be deleted later):
-    
-# =============================================================================
-# id - Unique ID. We can use this features as a count for other parameters.
-# 
-# CustomerID - Customer ID is not a unique ID, there are few customer IDs which have more than 80 repetitive occurrences.
-# 
-# Surname - This is the surname of the customer, there are too many repititions. Actually there are only around 2700 unique surnames.
-# 
-# Credit Score - Your creditworthiness is rated by a three-digit figure called a credit score. 300 to 850 is the range of FICO scores.
-# You have a better chance of getting approved for loans and better prices the higher your score. Now in our dataset there are range of values starting from 350 and going up to 850. Now this can be a very useful information while thinking about the churning.
-# 
-# Geography - There are 3 unique values - France, Spain, and Germany. One has to use Label Encoder or OneHotEncoder to encode these values.
-# 
-# Gender - There are only 2 unique values - Male and Female. Here a label binarizer is enough for the encoding purposes.
-# 
-# Age - Depicts the age of the customers. There are all possible values starting from 18 up to 92. 
-# There are 2 anomalies found in the age column - there 2 values in float - 32.44 and 36.44. It would be better if we can round those values to 32 and 36 respectively.
-# 
-# Tenure - It might show from how many years the customer has been related to the bank or may be vice versa. There are values ranging from 0 to 10.
-#  Most probably these values are in years.
-# 
-# Balance - This is the bank balance of the customer. There were many doubts in the discussion forum that the bank balance was 0. 
-# When I performed the analysis, I found that actually 89000+ people had 0 bank balance. While the maximum amount recorded was around 250,000.
-# 
-# Number of Products - Now this can be a very difficult question. While there are only 4 unique values possible - 1, 2, 3, and 4.
-#  This can be attributes to how many major/big products the customer owns. Or other explanation might be that how many products the customer has bought on loan.
-# 
-# Has Credit Card - Clear cut, whether the customer has a credit card or not. Same goes for the next column as well Is Active Member. 
-# 
-# Estimated Salary - What is the estimated salary of the individual. Now, this is a very important aspect of the real life scenario. Whenever you are given a credit from the bank,
-# they mostly ask for whether or not you are salaried. If you are estimated of getting a higher salary, easier for them to credit you a higher amount of loan.
-# =============================================================================
+# Fit the classifier on the training data
+nb_classifier.fit(x_train, y_train)
 
-#%%
+# Make predictions on the test data
+y_pred = nb_classifier.predict(x_train)
 
-# Look for nan values and categorical variables
+# Evaluate the accuracy of the classifier
+accuracy = accuracy_score(y_train, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
 
-train_df.info()
+# Display classification report
+print('\nClassification Report:')
+print(classification_report(y_train, y_pred))
 
-# No null values
+# Calculate AUC for estimating Kaggle Performance
 
-print(train_df.applymap(np.isreal).all(0))
+auc_score = roc_auc_score(y_train, y_pred)
+print(f'AUC: {auc_score:.2f}')
 
-# Identified Three Categorical Variables: Surname, Geography and Gender
+test_df_result = pd.DataFrame(test_df['id'])
 
-#%%
+test_pred = nb_classifier.predict(x_test)
 
-train_df.head()
+test_df_result['Exited'] = test_pred
 
-#%%
-
-#Inspect different attributes
-
-train_df.id.describe()
-
-# ID is unique, seems to be relating to process (distinct applicant list also possible)
-
-#%%
-
-len(train_df.CustomerId.unique())
-
-# 23221 distinct values -> not unique (reuse possible, or multiple processes per Customer)
-
-# To investigate, we look at rows with same CustomerId
-
-train_df['CustomerId'].duplicated()
-
-#df = train_df[train_df.duplicated('CustomerId', keep=False)].sort_values('CustomerId')
-
-# Customer ID does not display any obvious pattern or relation to the respective process
-
-# I will replace the CustomerID with the respective count
-
-CustomerIdCounts = train_df['CustomerId'].value_counts()
-
-# Create a new column with counts based on CustomerId
-train_df['CustomerIdCounts'] = train_df['CustomerId'].map(CustomerIdCounts)
-
-# Drop the old 'CustomerId' column if needed
-train_df.drop(columns=['CustomerId'], inplace=True)
-
-# Rename the new column to 'CustomerId'
-train_df.rename(columns={'CustomerIdCounts': 'CustomerId'}, inplace=True)
-
-# Potentially use a zscaler to transform
-
-from sklearn import preprocessing
-
-# create a scaler using Z score normalization / Standardization on the Fare attribute
-# The Fare varies greatly, thus we want to normalize this attribute
-zscore_scaler = preprocessing.StandardScaler().fit(train_df[['CustomerId']])
-
-#Apply the Zscore scaler Fare attribute and assign to a new Zscore column
-train_df['CustomerId_zscore']=zscore_scaler.transform(train_df[['CustomerId']])
-
-train_df['CustomerId_zscore'].describe()
-
-train_df.drop('CustomerId', axis=1, inplace=True)
-
-#%%
-
-# One-Hot encoding of gender
-
-train_df = pd.get_dummies(train_df, columns=['Gender'], drop_first=True)
-
-#%%
-
-# Classify names by their origin using corresponding lookup table
-
-from names_dataset import NameDataset, NameWrapper
-
-nd = NameDataset()
-
-#%%
-
-# Add new column with Country of surname origin
-
-def lookUpOrigin(row):
-    entry = nd.search(row['Surname'])
-    if(entry['last_name'] == None):
-        return 'Unknown'
-    return next(iter(nd.search(row['Surname'])['last_name']['country'].keys()))
-
-train_df['Name_Origin'] = train_df.apply(lookUpOrigin, axis=1)
-
-# Next I would like to add a column that encodes the country origin information
-# through its location data 
-
-from geopy.geocoders import Nominatim
-from sklearn.manifold import MDS
-from sklearn.preprocessing import MinMaxScaler
-from geopy.distance import geodesic
-
-# List of countries for name origins which we want to retrieve coordinates for
-countries = train_df['Name_Origin'].unique()
-
-# Initialize geolocator
-geolocator = Nominatim(user_agent="country_locator")
-
-coordinates = {}
-
-# Retrieve coordinates for each country
-for country in countries:
-    location = geolocator.geocode(country)
-    if location:
-        coordinates[country] = (location.latitude, location.longitude)
-    else:
-        print(f"Coordinates not found for {country}")
-
-# Create DataFrame from coordinates
-#coordinate_df = pd.DataFrame(coordinates.values(), index=coordinates.keys(), columns=['Latitude', 'Longitude'])
-
-# Calculate distances between countries
-# =============================================================================
-# distances = {}
-# for country1, coord1 in coordinates.items():
-#     for country2, coord2 in coordinates.items():
-#         if country1 != country2 and (country2, country1) not in distances:
-#             dist = geodesic(coord1, coord2).kilometers
-#             distances[(country1, country2)] = dist
-# =============================================================================
-
-# Create a distance matrix
-# =============================================================================
-# distance_matrix = pd.DataFrame(0, index=coordinate_df.index, columns=coordinate_df.index)
-# for (country1, country2), dist in distances.items():
-#     distance_matrix.loc[country1, country2] = dist
-#     distance_matrix.loc[country2, country1] = dist
-# =============================================================================
-
-# Apply Multidimensional Scaling (MDS) for dimensionality reduction
-# =============================================================================
-# mds = MDS(n_components=1, dissimilarity='precomputed', random_state=42)
-# numeric_values = mds.fit_transform(distance_matrix)
-# =============================================================================
-
-# Scale the numeric values to a desired range
-# =============================================================================
-# scaler = MinMaxScaler()
-# coordinate_df['NumericCountry'] = scaler.fit_transform(numeric_values)
-# =============================================================================
-
-# Replace value for unknown by average
-
-tuples = [value for key, value in coordinates.items() if key != 'Unknown']
-
-# Calculate the averages for each component of the tuples
-avg_1 = sum(x[0] for x in tuples) / len(tuples)
-avg_2 = sum(x[1] for x in tuples) / len(tuples)
-
-# Update the 'Unknown' key with the averages
-coordinates['Unknown'] = (avg_1, avg_2)
-
-train_df['Surname_Origin_Latitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[0])
-train_df['Surname_Origin_Longitude'] = train_df['Surname'].map(lambda x: coordinates.get(x, coordinates['Unknown'])[1])
-
-
-# NumericCountry does not reflect distance too well, instead I just insert Longitude and Latitute
-
-train_df['Geography_Latitude'] = train_df['Geography'].map(lambda x: coordinates[x][0])
-train_df['Geography_Longitude'] = train_df['Geography'].map(lambda x: coordinates[x][1])
-
-#%%
-
-# drop unneeded columns
-
-train_df.drop(labels=['Surname', 'Geography', 'Name_Origin'], axis=1, inplace=True)
+test_df_result.to_csv("Naive_Bayes_Model.csv", index=False)
 
