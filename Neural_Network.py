8495f58ccb0487675a67b9db11f593d201f4aff3
ClaasBeger <claas_baller@yahoo.com>
2024-01-16 05:53:37 +0100

    Tried further modifications for neural network, added real dataset, included first prediction with probabilities

--- Neural_Network.py
+++ Neural_Network.py
@@ -17,6 +17,14 @@ X = train_df.drop('Exited', axis=1)
 
 x_test = test_df
 
+reduced_train_df = train_df.drop(labels=['id'], axis = 1)
+
+reduced_test_df = test_df.drop(labels=['id'], axis = 1)
+
+y_train = reduced_train_df["Exited"]
+X = reduced_train_df.drop('Exited', axis=1)
+
+x_test = reduced_test_df
 
 # Standardize the features
 scaler = StandardScaler()
@@ -26,15 +34,14 @@ X[:] = scaler.fit_transform(X)
 X_tensor = torch.tensor(X.values, dtype=torch.float32)
 y_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
 
-class ChurnClassifier(nn.Module):
+class ChurnClassifierConvWrapper(nn.Module):
     
     def __init__(self, input_size, conv_channels=16):
-        super(ChurnClassifier, self).__init__()
+        super(ChurnClassifierConvWrapper, self).__init__()
         self.fc1 = nn.Linear(input_size, 32)
         self.relu = nn.ReLU()
         self.conv1 = nn.Conv1d(1, conv_channels, kernel_size=3, padding = 1)
-        self.fc2 = nn.Linear(conv_channels, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.fc2 = nn.Linear(conv_channels, input_size)
     
     def forward(self, x):
         x = self.fc1(x)
@@ -45,33 +52,61 @@ class ChurnClassifier(nn.Module):
         x = x.view(x.size(0), x.size(1), -1)  # Reshape for fully connected layer
         x = x.mean(dim=-1)  # Global average pooling
         x = self.fc2(x)
-        x = self.sigmoid(x)
         return x
     
 #%%
 
-class churnClassifier2H(nn.Module):
+class ChurnClassifier2HBlock(nn.Module):
     
     def __init__(self, input_size, dropout=0.2):
-        super(churnClassifier2H, self).__init__()
-        self.fc1 = nn.Linear(input_size, 32)
+        super(ChurnClassifier2HBlock, self).__init__()
         self.dropout = nn.Dropout(dropout)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
+        
+        self.fc1 = nn.Linear(input_size, 32)
+        self.ln1 = nn.LayerNorm(32)
         self.h1 = nn.Linear(32, 24)
-        self.h2 = nn.Linear(24, 16)
-        self.fc2 = nn.Linear(16, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.ln2 = nn.LayerNorm(24)
+        self.h2 = nn.Linear(24, input_size)
+        self.ln3 = nn.LayerNorm(input_size)
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.h1.weight)
+        nn.init.xavier_uniform_(self.h2.weight)
         
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.ln1(x)
         x = self.dropout(x)
         x = self.h1(x)
         x = self.relu(x)
+        x = self.ln2(x)
         x = self.dropout(x)
         x = self.h2(x)
         x = self.relu(x)
+        x = self.ln3(x)
         x = self.dropout(x)
+        return x
+    
+class ChurnClassifier2H(nn.Module):
+    
+    def __init__(self, input_size, dropout=0.2):
+        super().__init__()
+        self.block1 = ChurnClassifier2HBlock(input_size, dropout)
+        self.block2 = ChurnClassifierConvWrapper(input_size, conv_channels=15)
+        self.block3 = ChurnClassifier2HBlock(input_size, dropout)
+        self.block4 = ChurnClassifierConvWrapper(input_size, conv_channels=15)
+        self.block5 = ChurnClassifier2HBlock(input_size, dropout)
+        self.fc2 = nn.Linear(input_size, 1)
+        self.sigmoid = nn.Sigmoid()
+        nn.init.xavier_uniform_(self.fc2.weight)
+        
+    def forward(self, x):
+        x = x + self.block1(x)
+        x = x + self.block2(x)
+        x = x + self.block3(x)
+        x = x + self.block4(x)
+        x = x + self.block5(x)
         x = self.fc2(x)
         x = self.sigmoid(x)
         return x
@@ -85,7 +120,7 @@ criterion = nn.BCELoss()
 
 # Initialize the model
 input_size = X.shape[1]
-model = ChurnClassifier(input_size)
+model = ChurnClassifier2H(input_size)
 
 # Define the number of folds for cross-validation
 num_folds = 5
@@ -103,10 +138,15 @@ for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
     y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).view(-1, 1)
 
     # Initialize the model for each fold
-    model = ChurnClassifier(input_size)
+    model = ChurnClassifier2H(input_size)
 
     # Initialize optimizer for each fold
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    base_lr = 0.001
+    max_lr = 0.01
+    optimizer = optim.Adam(model.parameters(), lr=base_lr)
+    lr_schedule = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=base_lr,
+                                                    max_lr=max_lr, step_size_up=200, cycle_momentum=False)
+    
 
     # Training loop
     epochs = 1000
@@ -116,11 +156,19 @@ for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        lr_schedule.step()
+        
         if (epoch + 1) % 100 == 0:
-            print(f'Fold {fold + 1}, Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
+            model.eval()
+            with torch.no_grad():
+                y_pred_test = model(X_test_tensor)
+                y_pred_test_binary = (y_pred_test > 0.5).float()
+                accuracy = accuracy_score(y_test, y_pred_test_binary.numpy())
+            model.train()
+            print(f'Fold {fold + 1}, Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}, Val Accuracy: {accuracy:.2%}')
 
     # Evaluate the model on the test set for each fold
+    model.eval()
     with torch.no_grad():
         y_pred_test = model(X_test_tensor)
         y_pred_test_binary = (y_pred_test > 0.5).float()
@@ -155,18 +203,22 @@ def save_model(model):
 
 # Standardize the features
 scaler = StandardScaler()
-x_test[:] = scaler.fit_transform(test_df.copy())
+x_test[:] = scaler.fit_transform(x_test.copy())
 
 X_train_tensor = torch.tensor(X.values, dtype=torch.float32)
-y_train_tensor = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)
+y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
 X_test_tensor = torch.tensor(x_test.values, dtype=torch.float32)
 
 
 # Initialize the model for each fold
-model = ChurnClassifier(input_size)
+model = ChurnClassifier2H(X.shape[1])
 
 # Initialize optimizer for each fold
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+base_lr = 0.001
+max_lr = 0.01
+optimizer = optim.Adam(model.parameters(), lr=base_lr)
+lr_schedule = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=base_lr,
+                                                max_lr=max_lr, step_size_up=200, cycle_momentum=False)
 
 # Training loop
 epochs = 1000
@@ -176,20 +228,22 @@ for epoch in range(epochs):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+    lr_schedule.step()
 
     if (epoch + 1) % 100 == 0:
         print(f'Fold {fold + 1}, Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
 
 #%%
 
-# Evaluate the model on the test set for each fold
+# Evaluate the model
+model.eval()
 with torch.no_grad():
-    y_pred_test = (model(X_test_tensor) > 0.5).int()
+    y_pred_test = model(X_test_tensor)
 
 test_df_result = pd.DataFrame(test_df['id'])
 
 test_df_result['Exited'] = y_pred_test
 
-test_df_result.to_csv("TwoLayer_NCN_Model.csv", index=False)
+test_df_result.to_csv("NN_3B_Adam_CyclingLR_Model_probs.csv", index=False)
 
 
